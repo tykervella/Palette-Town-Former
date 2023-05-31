84fa92aa965204b53a9cd6e255568e27b3dde160
tykervella <kervellatyler@gmail.com>
2023-05-30 19:28:09 -0700

    added mongo db specifics to OUR project

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,22 +1,22 @@
 const db = require('../config/connection');
-const { User, Thought } = require('../models');
+const { User, Deck } = require('../models');
 const userSeeds = require('./userSeeds.json');
-const thoughtSeeds = require('./thoughtSeeds.json');
+const deckSeeds = require('./deckSeeds.json');
 
 db.once('open', async () => {
   try {
-    await Thought.deleteMany({});
+    await Deck.deleteMany({});
     await User.deleteMany({});
 
     await User.create(userSeeds);
 
-    for (let i = 0; i < thoughtSeeds.length; i++) {
-      const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
+    for (let i = 0; i < deckSeeds.length; i++) {
+      const { _id, deckOwner } = await Deck.create(deckSeeds[i]);
       const user = await User.findOneAndUpdate(
-        { username: thoughtAuthor },
+        { username: deckOwner },
         {
           $addToSet: {
-            thoughts: _id,
+            decks: _id,
           },
         }
       );
