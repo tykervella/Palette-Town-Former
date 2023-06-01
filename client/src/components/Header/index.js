58ba4b1e4cbceb8ea0198a265cc8e700777c37ce
marleyschneiderr <marley33@uw.edu>
2023-05-31 20:43:13 -0700

    Adding navbar

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,21 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import Auth from '../../utils/auth';
+import Auth from "../../utils/auth";
 
 const Header = () => {
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
+
   return (
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
           <Link className="text-light" to="/">
-            <h1 className="m-0">Tech Thoughts</h1>
+            <h1 className="m-0">Palette Town</h1>
           </Link>
-          <p className="m-0">Get into the mind of a programmer.</p>
+          <p className="m-0">Where Hexcodes and Pokémon Collide</p>
+        </div>
+        <div className="navbar navbar-expand-lg navbar-light text-white">
+          <ul className="navbar-nav mx-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/page1">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/page2">
+                Create
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/page3">
+                Marketplace
+              </Link>
+            </li>
+          </ul>
         </div>
         <div>
           {Auth.loggedIn() ? (
