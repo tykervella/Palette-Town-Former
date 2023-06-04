b3d06c4397a0728914419387407bc1623cf25ca0
Your Name <terrimack15@gmail.com>
2023-06-03 19:43:11 -0700

    organizing home.js. Added content to caughtdecks.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -14,19 +14,18 @@ const Home = () => {
   return (
     // primary container for entire page
     <div>
-    <Container>
+    <Container className=''>
 
       {/* banner */}
       <Row>
         <Col xs='auto' className='border border-black rounded-xl bg-red-200 p-16 m-2'>
           <Row className='text-center'>
-              <h2 className='text-3xl font-bold text-white text-center'>Banner Section</h2>
+              <h2 className='text-3xl font-bold text-white'>Banner Section</h2>
           </Row>
 
           <Row className='float-right'>
-            <button className='border border-black rounded-xl bg-red-400'>Go To MarketPlace</button>
+            <button className='border border-black rounded-xl bg-red-400 p-1'>Go To MarketPlace</button>
           </Row>
-
         </Col>
       </Row>
 
@@ -43,7 +42,8 @@ const Home = () => {
         {/* caught decks */}
         <Col xs="auto" className='border border-black rounded-xl bg-slate-400 p-16 m-2'>
           <h2 className='text-3xl font-bold text-white text-center'>Caught Decks</h2>
-            <div></div>
+            <div><CaughtDecks/></div>
+            
         </Col>
         {/* top card listings */}
         <Col xs="3" className='border border-black rounded-xl bg-slate-400 p-16 m-2'>
