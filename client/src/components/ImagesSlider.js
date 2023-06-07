177ee5084a9c4d3155fd48192f5619c72156bc26
Your Name <terrimack15@gmail.com>
2023-06-06 21:30:31 -0700

    worked on restructuring login.signup page. working on carousel

--- client/src/components/ImagesSlider.js
+++ client/src/components/ImagesSlider.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { SliderData } from './SliderData';
-import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
 const ImageSlider = ({ slides }) => {
   const [current, setCurrent] = useState(0);
@@ -14,27 +14,31 @@ const ImageSlider = ({ slides }) => {
     setCurrent(current === 0 ? length - 1 : current - 1);
   };
 
-  if (!Array.isArray(slides) || slides.length <= 0) {
+  if (!Array.isArray(slides) || slides.length === 0) {
     return null;
   }
 
   return (
-    <section className='slider'>
-      <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide} />
-      <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
-      {SliderData.map((slide, index) => {
-        return (
-          <div
-            className={index === current ? 'slide active' : 'slide'}
-            key={index}
-          >
-            {index === current && (
-              <img src={slide.image} alt='travel image' className='image' />
-            )}
-          </div>
-        );
-      })}
-    </section>
+    <div className="slider">
+      <button className="prev-btn" onClick={prevSlide}>
+        <FiChevronLeft />
+      </button>
+
+      {slides.map((slide, index) => (
+        <div
+          className={index === current ? 'slide active' : 'slide'}
+          key={index}
+        >
+          {index === current && (
+            <img src={slide.image} alt='travel image' className='image' />
+          )}
+        </div>
+      ))}
+
+      <button className="next-btn" onClick={nextSlide}>
+        <FiChevronRight />
+      </button>
+    </div>
   );
 };
 
