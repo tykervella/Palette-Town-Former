c16305b13c78589c6faadc079455f8c1dfa23466
Your Name <terrimack15@gmail.com>
2023-06-09 20:35:26 -0700

    added drop down menu on create page for starter decks

--- client/src/pages/CreateDeck.js
+++ client/src/pages/CreateDeck.js
@@ -4,6 +4,8 @@ import Auth from '../utils/auth';
 import { useNavigate } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { CREATE_DECK } from '../utils/mutations';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 const CreateDeck = () => {
   const token = Auth.getToken();
@@ -52,13 +54,13 @@ const CreateDeck = () => {
   return (
     <Container className="flex justify-center m-10">
       <Row>
-        <Col className="max-w-3xl w-full p-8 bg-[#4B957E] text-white rounded-lg shadow-lg">
+        <Col className="max-w-3xl w-full p-4 bg-[#4B957E] text-white rounded-lg shadow-lg">
           <div className='border-2 border-[#FFEC99] p-2 rounded-lg'>
             <h2 className="text-4xl font-bold m-6 underline">Create your Pokemon Deck</h2>
             <div className="text-center">
               <Form className="text-black mx-auto">
                 <div className="w-80 mx-auto">
-                  <Form.Group className="box-shadow-xl p-4 rounded-lg">
+                  <Form.Group className="box-shadow-xl p-2 rounded-lg">
                     <Form.Control
                       type="text"
                       id="name"
@@ -72,11 +74,22 @@ const CreateDeck = () => {
                 </div>
 
                 {/* bottom portion of create page */}
-                <div>
+                <div className='flex flex-row align-items-center justify-center pb-4'>
                   <button onClick={handleCreateDeck} className="bg-[#FFEC99] hover:bg-[#AFD7CA] text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                     Create Deck
                   </button>
                   <span className='ml-2 mr-2'>OR</span>
+
+                  <NavDropdown title="Starter Decks" id="basic-nav-dropdown" className="bg-[#FFEC99] hover:bg-[#AFD7CA] text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                    <NavDropdown.Item>None</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    {/* starter deck options */}
+                    <NavDropdown.Item>Fire</NavDropdown.Item>
+                    <NavDropdown.Item>Water</NavDropdown.Item>
+                    <NavDropdown.Item>Grass</NavDropdown.Item>
+                    <NavDropdown.Item>Dragon</NavDropdown.Item>
+
+                  </NavDropdown>
                 </div>
 
               </Form>
