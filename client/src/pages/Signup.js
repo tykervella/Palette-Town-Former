879d6b745bfe7a4ad2195621e028e20cd5f328c4
marleyschneiderr <marley33@uw.edu>
2023-06-11 17:54:49 -0700

    small changes to login/sign up

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,18 +1,23 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import { ADD_USER } from '../utils/mutations';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../utils/mutations";
+import { BsBrush } from "react-icons/bs";
 
-import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 
 const Signup = () => {
-  const [formState, setFormState] = useState({ username: '', email: '', password: '', });
+  const [formState, setFormState] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
   const [showPassword, setShowPassword] = useState(false); // New state variable
 
   const [addUser, { error, data }] = useMutation(ADD_USER);
@@ -44,34 +49,39 @@ const Signup = () => {
   };
 
   return (
-    <Container className='mb-5'>
-      <Row>
-
+    <Container className="mb-5">
+      <Row className="justify-content-center align-items-center">
         {/* left column */}
-        <Col md={7} className='d-none d-md-block'>
-          <div className='loginLogo rounded-3xl shadow-xl'>
-            <div className='d-flex flex-column align-items-center text-center pt-60'>
-              <h1></h1>
+        <Col md={7} className="d-none d-md-block">
+          <div className=" text-[#0B3C49] loginLogo rounded-3xl shadow-xl">
+            <div className="d-flex flex-column align-items-center justify-center text-center pt-60">
+              <h1 className="mb-2 mt-4">Sign up to Enter Palette Town</h1>
+              <h2 className="text-center italic">
+                Where hexcodes and palettes collide{" "}
+                <span className="d-inline-flex align-items-center">
+                  <BsBrush size={42} style={{ color: "#0B3C49" }} />
+                </span>
+              </h2>
             </div>
           </div>
         </Col>
 
         {/* right column */}
-        <Col md={5} className='bg-[#0B3C49]  rounded-3xl shadow-xl'>
-          <h1 className='text-white pt-10 text-center'>Palette Town</h1>
-          <h2 className='text-white pt-20 text-center'>Where Hexcodes Choose YOU!</h2>
+        <Col md={5} className="bg-[#0B3C49] rounded-3xl shadow-xl">
+          <h1 className="text-white pt-10 text-center mt-4">Palette Town</h1>
+          <h2 className="text-xl text-white pt-2 text-center italic">
+            Where Hexcodes Choose YOU!
+          </h2>
           {data ? (
             <p>
-              Success! You may now head{' '}
+              Success! You may now head{" "}
               <Link to="/">back to the homepage.</Link>
             </p>
-
           ) : (
-
-            <div className='d-flex justify-content-center align-items-center justify-content-center'>
+            <div className="d-flex justify-content-center align-items-center justify-content-center">
               <form onSubmit={handleFormSubmit}>
-                <div className='mb-3 pt-20'>
-                  <p className='text-white  text-xs'>New Username</p>
+                <div className="mb-3 pt-20">
+                  <p className="text-white  text-xs">New Username</p>
                   <input
                     className="form-input bg-transparent border-b-2 border-[#376D5B] w-full text-white focus:outline-none resize-none"
                     placeholder="Your username"
@@ -82,8 +92,8 @@ const Signup = () => {
                   />
                 </div>
 
-                <div className='mb-3'>
-                  <p className='text-white text-xs'>User Email</p>
+                <div className="mb-3">
+                  <p className="text-white text-xs">User Email</p>
                   <input
                     className="form-input bg-transparent border-b-2 border-[#376D5B] w-full text-white focus:outline-none resize-none"
                     placeholder="example@example.com"
@@ -94,13 +104,13 @@ const Signup = () => {
                   />
                 </div>
 
-                <div className='mb-3'>
-                  <p className='text-white text-xs'>Password</p>
+                <div className="mb-4">
+                  <p className="text-white text-xs">Password</p>
                   <input
                     className="form-input bg-transparent border-b-2 border-[#376D5B] w-80 text-white focus:outline-none resize-none"
                     placeholder="******"
                     name="password"
-                    type={showPassword ? 'text' : 'password'}
+                    type={showPassword ? "text" : "password"}
                     value={formState.password}
                     onChange={handleChange}
                   />
@@ -111,29 +121,34 @@ const Signup = () => {
                     onChange={toggleShowPassword}
                     className="hidden"
                   />
-                  <label htmlFor="showPassword" className="ml-2 icon-label text-xl text-white " style={{ cursor: 'pointer' }}>
-                    {showPassword ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
+                  <label
+                    htmlFor="showPassword"
+                    className="ml-2 icon-label text-xl text-white "
+                    style={{ cursor: "pointer" }}
+                  >
+                    {showPassword ? (
+                      <AiOutlineEyeInvisible />
+                    ) : (
+                      <AiOutlineEye />
+                    )}
                   </label>
                 </div>
 
-                <div className='flex justify-center'>
+                <div className="flex justify-center">
                   <button
-                    className="mb-4 btn-block bg-[#FFEC99] hover:bg-[#4B957E] font-bold py-2 px-4 rounded-lg"
-                    style={{ cursor: 'pointer' }}
-                    type="submit">
+                    className=" text-black mt-2 mb-4 btn-block bg-[#FFEC99] hover:bg-[#4B957E] font-bold py-3 px-6 rounded-lg text-lg"
+                    style={{ cursor: "pointer" }}
+                    type="submit"
+                  >
                     Sign Up
                   </button>
                 </div>
-
               </form>
             </div>
           )}
 
           {error && (
-            <div className="my-3 p-3 bg-danger text-white">
-              {error.message}
-            </div>
-
+            <div className="my-3 p-3 bg-danger text-white">{error.message}</div>
           )}
         </Col>
       </Row>
