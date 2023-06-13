9037d53d1ab12e780104240a4bf0c6aed4bfdfff
tykervella <kervellatyler@gmail.com>
2023-06-12 17:07:54 -0700

    stripe set up

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useApolloClient } from '@apollo/client';
-import { useNavigate } from 'react-router-dom';
 import Auth from "../utils/auth";
 import { Container, Row, Col } from "react-bootstrap";
+import { loadStripe } from "@stripe/stripe-js";
+
 
 import { GET_CART } from '../utils/queries';
 import CartItem from '../components/CartItem';
+
 
 const Checkout = () => {
   const token = Auth.getToken();
@@ -13,7 +15,8 @@ const Checkout = () => {
   const [cartItems, setCartItems] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
   const client = useApolloClient();
-  const navigate = useNavigate();
+  const stripePromise = loadStripe("pk_test_51NIKBUE8H0olH7rAq4x67BpaEWrBjgaHUU1VrfPUQt0ANI6FXL21E8vvwLzeAIDdZQrAfZFv2AQ8Qx3U2UWdgLor00bYZryjyK");
+
 
 
 
@@ -43,6 +46,36 @@ const Checkout = () => {
     }
   }, [username, client]);
 
+  const handleCheckout = async () => {
+    // Load Stripe.js
+    const stripe = await stripePromise;
+  
+    // Call your backend server to create a Checkout Session
+    const response = await fetch("http://localhost:3001/create-checkout-session", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        cart: cartItems
+      })
+    });
+  
+    const session = await response.json();
+  
+    // When the customer clicks on the button, redirect them to Checkout.
+    const result = await stripe.redirectToCheckout({
+      sessionId: session.id,
+    });
+  
+    if (result.error) {
+      // If `redirectToCheckout` fails due to a browser or network
+      // error, display the localized error message to your customer
+      console.error(result.error.message);
+    }
+  };
+  
+
 
   return (
     <Container className="mt-5 text-center shadow-lg mb-4" style={{ backgroundColor: 'rgba(255, 255, 255, 0.8)' }}>
@@ -68,6 +101,9 @@ const Checkout = () => {
       <div className="d-flex justify-content-end p-4">
         <h5 className="mr-3">Total: <span className="text-[#4B957E]">${totalPrice}</span></h5>
       </div>
+
+      <button onClick={handleCheckout} className="checkout-button">Checkout</button>
+
     </Container>
   );
 }
